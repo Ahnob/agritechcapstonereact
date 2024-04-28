97c65c2fa1327d006c5f0db30e0fffd836fa7aa3
Anob <anobtakon@gmail.com>
2024-04-28 12:59:31 +0100

    started the subscription dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Home from "./components/Home/Home";
 import AccountVerify from "./components/Accountverify/AccountVerify";
 import Signup from "./components/Signup/Signup";
 import Settings from "././components/Dashboard/Settings/Settings";
+import Subscription from "./components/Dashboard/Subscription/Subscription";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/settings" element={<Settings />} />
-
+        <Route path="/subscription" element={<Subscription />} />
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
--- src/components/Dashboard/Subscription/Subscription.jsx
+++ src/components/Dashboard/Subscription/Subscription.jsx
@@ -1,41 +1,133 @@
 import "./Subscription.css";
+import { useState } from "react";
+import firstimage from "../../../assets/pikaso_texttoimage_A-3d-model-of-a-triangular-vertical-farm (1) 2.svg";
 
 function Subscription() {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
-    <div>
-      <div className="top-container">
-        <h3 className="setting-header">Settings</h3>
-        <h3> Manage your account settings</h3>
+    <>
+      <div className="second-container">
+        <div className="top-container">
+          <h3 className="setting-header">Settings</h3>
+          <h3> Manage your account settings</h3>
+        </div>
+        <div className="flex-card">
+          <div className="subscribe-container">
+            <div className="first-card">
+              <img src={firstimage} alt="First" />
+              <h3>Agriculture Farming Model </h3>
+            </div>
+            <div>
+              <h3>
+                $20
+                <span> per user/month</span>
+              </h3>
+            </div>
+            <div className="subscribe-card">
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    value="option1"
+                    checked={selectedOption === "option1"}
+                    onChange={handleOptionChange}
+                  />
+                  <span className="checkmark"></span>
+                  An agriculture farming model a strategic framework and
+                  approach used by farmers to cultivate crops and raise
+                  livestock efficiently.
+                </label>
+              </div>
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    value="option2"
+                    checked={selectedOption === "option2"}
+                    onChange={handleOptionChange}
+                  />
+                  <span className="checkmark"></span>
+                  Access to Courses and resources on Vertical farming
+                </label>
+              </div>
+
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    value="option3"
+                    checked={selectedOption === "option3"}
+                    onChange={handleOptionChange}
+                  />
+                  <span className="checkmark"></span>
+                  Access to Courses and resources on Vertical farming
+                </label>
+              </div>
+            </div>
+            <button className="subscribe-btn">Subscribe</button>
+          </div>
+          <div className="subscribe-container">
+            <div className="first-card">
+              <img src={firstimage} alt="First" />
+              <h3>Agriculture Farming Model </h3>
+            </div>
+            <div>
+              <h3>
+                $20
+                <span> per user/month</span>
+              </h3>
+            </div>
+            <div className="subscribe-card">
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    value="option1"
+                    checked={selectedOption === "option1"}
+                    onChange={handleOptionChange}
+                  />
+                  <span className="checkmark"></span>
+                  An agriculture farming model a strategic framework and
+                  approach used by farmers to cultivate crops and raise
+                  livestock efficiently.
+                </label>
+              </div>
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    value="option2"
+                    checked={selectedOption === "option2"}
+                    onChange={handleOptionChange}
+                  />
+                  <span className="checkmark"></span>
+                  Access to Courses and resources on Vertical farming
+                </label>
+              </div>
+
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    value="option3"
+                    checked={selectedOption === "option3"}
+                    onChange={handleOptionChange}
+                  />
+                  <span className="checkmark"></span>
+                  Access to Courses and resources on Vertical farming
+                </label>
+              </div>
+            </div>
+            <button className="subscribe-btn">Subscribe</button>
+          </div>
+        </div>
       </div>
-      <div className="subscribe-container">
-        <div className="first-card">
-          <img src={firstimage} />
-          <h3>Agriculture Farming Model </h3>
-        </div>
-        <div>
-          <h3>
-            $20
-            <span> per user/month</span>
-          </h3>
-        </div>
-        <div className="subscribe-card">
-          <label>
-            <input type="radio" value="option1" />
-            An agriculture farming model a strategic framework and approach used
-            by farmers to cultivate crops and raise livestock efficiently.
-          </label>
-          <label>
-            <input type="radio" value="option2" />
-            Access to Courses and resources on Vertical farming
-          </label>
-          <label>
-            <input type="radio" value="option3" />
-            Access to Courses and resources on Vertical farming
-          </label>
-        </div>
-        <div className="second-card"></div>
-      </div>
-    </div>
+    </>
   );
 }
 
