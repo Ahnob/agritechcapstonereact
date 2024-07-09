847febc4885dcf1da9313a910a0967a4e61413da
Anob <anobtakon@gmail.com>
2024-07-09 23:42:32 +0100

    made some adjustments to filter

--- src/dashboard/Marketplace.jsx
+++ src/dashboard/Marketplace.jsx
@@ -1,9 +1,11 @@
+import FilterButton from "../components/filter/FilterButton";
 import SearchInput from "../components/searchinput/SearchInput";
 
 function MarketplaceDashboard() {
   return (
     <div>
       <SearchInput />
+      <FilterButton />
     </div>
   );
 }
