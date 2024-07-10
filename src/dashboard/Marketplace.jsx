fddbdfda02308f28ea2d9a7cd428f1a3bb006293
Anob <anobtakon@gmail.com>
2024-07-10 23:56:20 +0100

    working on the marketplace dashboard

--- src/dashboard/Marketplace.jsx
+++ src/dashboard/Marketplace.jsx
@@ -1,11 +1,21 @@
 import FilterButton from "../components/filter/FilterButton";
 import SearchInput from "../components/searchinput/SearchInput";
+import Cart from "../assets/cart_icon.svg";
 
 function MarketplaceDashboard() {
   return (
     <div>
-      <SearchInput />
-      <FilterButton />
+      <div>
+        <div>
+          <img src={Cart} alt="cart-icon" />
+          <p>Market place</p>
+        </div>
+      </div>
+
+      <div>
+        <SearchInput />
+        <FilterButton />
+      </div>
     </div>
   );
 }
